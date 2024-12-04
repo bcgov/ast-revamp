c6d34cf63f58746bb8560ca9487b304a3c4b288d
IDIR\ISAACAVE <isaac.cave@gov.bc.ca>
2024-12-04 11:52:19 -0800

    Initial pseudo-code

---
+++ processing.py
@@ -0,0 +1,46 @@
+'''
+Automated Status Tool Successor
+
+Description:
+
+Inputs:
+
+Processing:
+
+Outputs:
+
+Next Steps:
+
+Authors:
+
+
+'''
+# %% Imports
+
+
+# %% Modules
+
+
+# Class Call_Routine(): # effectively main, calls other classes and functions
+    # Init
+    # maintenance (creating folders) # Put in separate function/class
+    # Calls to other functions
+
+
+# Class Data_integrity():
+    # Init
+        # inputs
+    # number_features
+    # Output
+
+
+# Class Oracle_authentication():
+    # 
+
+
+# Class Intersect():
+    # 
+
+
+# Class Mapping():
+    #
